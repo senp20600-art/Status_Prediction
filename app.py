c4cd2c7c5a5cf90a3e34f080349fdc49b8a41263
senp20600-art <senp20600@gmail.com>
2026-05-22 18:25:44 +0530

    Refactor input fields for country status prediction

--- app.py
+++ app.py
@@ -5,17 +5,6 @@ status_prediction_model = joblib.load('status_prediction_model.pkl')
 st.title('Country Status Prediction')
 year = st.number_input('Year', min_value=2000, max_value=2015, step=1)
 population = st.number_input('Population', min_value=30)
-country=st.selectbox('Select Country', ['Afghanistan', 'Albania', 'Algeria', 'Angola', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize',
-                                'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei',
-                                'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde',
-                                'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros',
-                                'Congo (Brazzaville)', 'Congo (Kinshasa)', 'Costa Rica', "Cote d'Ivoire", 'Croatia',
-                                'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica',
-                                'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea',
-                                'Eritrea', 'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France',
-                                "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada",
-                                "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras",
-                                "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland"])
 gdp_per_capita = st.number_input('GDP', min_value=1.00)
 life_expectancy = st.number_input('Life Expectancy', min_value=35.00)
 adult_mortality = st.number_input('Adult Mortality', min_value=0.00,max_value=800.00)
@@ -35,7 +24,7 @@ thinness_5_9_years = st.number_input('thinness 5-9 years', min_value=0.00,max_va
 income_composition_of_resources = st.number_input('Income composition of resources', min_value=0.00,max_value=1.00)
 schooling = st.number_input('Schooling', min_value=0.00,max_value=21.00)
 if st.button('Predict Status'):
-    input_data = [[year, population, gdp_per_capita,country, life_expectancy, adult_mortality, infant_deaths, alcohol,
+    input_data = [[year, population, gdp_per_capita,life_expectancy, adult_mortality, infant_deaths, alcohol,
                    percentage_expenditure, hepatitis_b, measles, under_five_deaths, polio, total_expenditure,
                    diphtheria, hiv_aids, body_mass_index, thinness_1_19_years, thinness_5_9_years, income_composition_of_resources, schooling]]
     prediction = status_prediction_model.predict(input_data)
